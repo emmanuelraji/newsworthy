2ea0cc1e5a84f87cf46e643e5f978bcdf16d7476
Emmanuel Raji <emmanuelraji10@gmail.com>
2022-04-19 09:53:03 -0400

    Added images from api

--- main.js
+++ main.js
@@ -1,50 +1,60 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("#search-form");
-  const input = document.querySelector("#news-input");
-  const root = document.getElementById("root");
+let NEWS_API_KEY;
 
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const term = input.value;
-    const URL = `https://newsapi.org/v2/everything?q=${term}&apiKey=1e0c3c9c8adc4aa9a6c5322e7a576f17`;
-    const newsList = await fetchNews(URL);
-    const div = displayNews(newsList);
-    root.appendChild(div);
-  });
+// window.addEventListener("load", () => {
+const form = document.querySelector("#search-form");
+const input = document.querySelector("#news-input");
+const articles = document.getElementById("articles");
 
-  function displayNews(news) {
-    const divNews = document.createElement("div");
-    divNews.classList.add("news-container");
-    news.forEach((article) => {
-      const div = document.createElement("div");
-      div.classList.add("news-item");
-      const title = document.createElement("h2");
-      title.textContent = article.title;
-      const desc = document.createElement("p");
-      desc.textContent = article.description;
-      const auth = document.createElement("p");
-      auth.textContent = article.author;
-      div.appendChild(title);
-      div.appendChild(desc);
-      div.appendChild(auth);
-      divNews.appendChild(div);
-    });
-    return divNews;
-  }
-
-  async function fetchNews(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      const json = await response.json();
-      const data = await json.articles;
-      console.log(data);
-      return data;
-      //return data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const term = input.value;
+  const URL = `https://newsapi.org/v2/everything?q=${term}&apiKey=${NEWS_API_KEY}`;
+  const newsList = await fetchNews(URL);
+  const div = displayNews(newsList);
+  articles.appendChild(div);
 });
+
+function displayNews(news) {
+  const divNews = document.createElement("div");
+  divNews.classList.add("news-container");
+  news.forEach((article) => {
+    const div = document.createElement("div");
+    div.classList.add("news-item");
+    const newsText = document.createElement("div");
+    newsText.classList.add("news-text");
+    const title = document.createElement("h2");
+    title.textContent = article.title;
+    newsText.appendChild(title);
+    const desc = document.createElement("p");
+    desc.textContent = article.description;
+    newsText.appendChild(desc);
+    const auth = document.createElement("p");
+    auth.textContent = article.author;
+    newsText.appendChild(auth);
+    const img = document.createElement("img");
+    img.src = article.urlToImage;
+    img.alt = "article related img";
+    img.classList.add("article-img");
+    div.appendChild(newsText);
+    div.appendChild(img);
+    divNews.appendChild(div);
+  });
+  return divNews;
+}
+
+async function fetchNews(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const json = await response.json();
+    const data = await json.articles;
+    console.log(data);
+    return data;
+    //return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+// });
